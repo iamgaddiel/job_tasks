ed866f86b66f43ca79a72699aa0dcf6884bd8be1
iamgaddiel <ighotagaddiel98@gmail.com>
2022-09-12 01:31:10 -0400

    added api implementation for programitcally add ip for access list

--- flask_template_task/app/__init__.py
+++ flask_template_task/app/__init__.py
@@ -1,4 +1,10 @@
-from flask import Flask, jsonify, request
+import requests
+
+from flask import Flask, jsonify
+from requests.auth import HTTPDigestAuth
+# from ipify import get_ip
+# from ipify.exceptions import ConnectionError, ServiceError
+
 
 from config import Config
 from extensions import flask_jwt_ext
@@ -20,8 +26,45 @@ def create_app(config=Config):
     app.register_blueprint(template_bp)
     app.register_blueprint(auth_bp)
 
+    atlas_group_id = app.config.get("ATLAS_GROUP_ID")
+    atlas_api_public_key = app.config.get("ATLAS_PUBLIC_KEY")
+    atlas_api_private_key = app.config.get("ATLAS_PRIVATE_KEY")
+    # ip = requests.get('https://api.ipify.org').text
+    # print(ip, '<----------- Ip address')
+
+    print(atlas_api_private_key)
+
+    # whitelist current IP adress via API Key on MongoDB Atlas
+    print("Whitelisting...")
+
+    # alternative to receive external ip-adress: https://checkip.amazonaws.com, https://ident.me 
+    ip = requests.get('https://ident.me').text.strip()
+
+    print(ip)
+
+    resp = requests.post(
+        "https://cloud.mongodb.com/api/atlas/v1.0/groups/{groupId}/accessList".format(groupId=atlas_group_id),
+        # auth=HTTPDigestAuth(atlas_api_public_key, atlas_api_private_key),
+        json={'ipAddress': ip, 'comment': 'From home'}
+    )
+
+    if resp.status_code in (200, 201):
+        print("MongoDB Atlas whitelist request successful", flush=True)
+    else:
+        print(
+            "MongoDB Atlas whitelist request problem: status code was {status_code}, content was {content}".format(
+                status_code=resp.status_code, content=resp.content
+            ),
+
+            flush=True
+        )
+
+
+
     @app.route('/', methods=['GET'])
     def root():
         return jsonify(data={"data": "Python Json Task"})
+        
 
     return app
+
--- flask_template_task/config.py
+++ flask_template_task/config.py
@@ -2,5 +2,9 @@
 class Config():
     """ Holds all configurations for the project """
     MONGO_URI = "mongodb+srv://iamgaddiel:iamgaddiel@tempaltecluster.nrhzd2g.mongodb.net/?retryWrites=true&w=majority"
-    SECRET_KEY = "c00e4c66c7bb4f5c2216afad90de224ce716a677",
+    SECRET_KEY = "c00e4c66c7bb4f5c2216afad90de224ce716a677"
     JWT_SECRET_KEY = "acdefghijklmnopqrstuvwxyz"
+    ATLAS_PRIVATE_KEY = "4a56d1d-5a22-4834-9e47-32ba176a8a81"
+    ATLAS_PUBLIC_KEY =  "obeensuf"
+    ATLAS_GROUP_ID = "62f3e001e9e14b3af85400ee"
+    
